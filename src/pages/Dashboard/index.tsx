581637e12d3359b333efd9ebfa8cd822e34a241d
Rogério Sandim <sandimroger@gmail.com>
2020-09-16 17:13:13 +0100

    feat: Add connection with git API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,36 +1,65 @@
-import React from 'react';
-import logoImg from '../../assets/logo.svg';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
-      <img src={logoImg} alt='Github Explorer' />
+      <img src={logoImg} alt="Github Explorer" />
       <Title>Explore Github repositories</Title>
 
-      <Form action=''>
-        <input placeholder='Type the repository name' />
-        <button type='submit'>Search</button>
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Type the repository name"
+        />
+        <button type="submit">Search</button>
       </Form>
 
       <Repositories>
-        <a href='Test'>
-          <img
-            src='https://avatars2.githubusercontent.com/u/12728533?s=460&u=6eec90d75219f95f584736404f3f3c4314c12a28&v=4'
-            alt='Rogerio Sandim'
-          />
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="Test">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
 
-          <div>
-            <strong>rogeriosandim/mockup-udacity-project</strong>
-            <p>
-              This is the first project that I've done during the Web Front-End
-              Foundations Nanodegree Program at Udacity
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
